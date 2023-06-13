e986e1876fb14ee0f768cf04ee9f6a00d73bf7f6
Cory House <housecor@gmail.com>
2023-06-13 11:04:20 -0500

    add comment

--- src/remoteTypes.d.ts
+++ src/remoteTypes.d.ts
@@ -1,5 +1,6 @@
 ///<reference types="react" />
 
+// TypeScript doesn't know what the remote module resolves to, so specify the types here.
 declare module "app1/CounterAppOne" {
   const CounterAppOne: React.ComponentType;
   export default CounterAppOne;

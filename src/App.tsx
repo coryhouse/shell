e5d1778c0fdcd1de5883b385aab8260e08cc6019
Cory House <housecor@gmail.com>
2023-06-13 06:49:35 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { lazy } from "react";
+import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ShellApp } from "./components/ShellApp";
 
 const CounterAppOne = lazy(() => import("app1/CounterAppOne"));
 const CounterAppTwo = lazy(() => import("app2/CounterAppTwo"));
 
-export function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <ShellApp
-            CounterAppOne={CounterAppOne}
-            CounterAppTwo={CounterAppTwo}
-          />
-        }
-      />
-      <Route path="app1/*" element={<CounterAppOne />} />
-      <Route path="app2/*" element={<CounterAppTwo />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ShellApp
+              CounterAppOne={CounterAppOne}
+              CounterAppTwo={CounterAppTwo}
+            />
+          }
+        />
+        <Route path="app1/*" element={<CounterAppOne />} />
+        <Route path="app2/*" element={<CounterAppTwo />} />
+      </Routes>
+    </>
   );
 }
--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { App } from "./App";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root") as HTMLElement).render(

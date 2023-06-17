4c3fc7526c72daa8dc626a99765d645adea2ffaf
Cory House <housecor@gmail.com>
2023-06-17 09:12:15 -0500

    Extract types

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+// TODO: Publish this type via npm so it can used by each remote.
+// This is the API that the shell enforces on each remote.
+export type RemoteProps = {
+  parentCount: number;
+  urls: Url;
+  user: User;
+};
+
+export type Url = Record<"about" | "home", string>;
+export type User = { id: number; name: string };

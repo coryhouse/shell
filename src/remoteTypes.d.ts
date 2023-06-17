e71e1c074d3eda41c366bd9ba403bfb3d1660ef0
Cory House <housecor@gmail.com>
2023-06-17 09:09:49 -0500

    Call central types

--- src/remoteTypes.d.ts
+++ src/remoteTypes.d.ts
@@ -1,18 +1,11 @@
 ///<reference types="react" />
 
-// TypeScript doesn't know what the remote module resolves to, so specify the types here.
+// TypeScript doesn't know what the remote module resolves to, so we must specify each remote's types.
+// TODO: Can each remote's types be read or generated?
 declare module "remote1/RemoteOne" {
-  // TODO: Eliminate copy/pasted Remote props below
-  export default React.ComponentType<{
-    parentCount: number;
-    nav: (url: string) => void;
-  }>;
+  export default React.ComponentType<import("./types").RemoteProps>;
 }
 
 declare module "remote2/RemoteTwo" {
-  // TODO: Eliminate copy/pasted Remote props below
-  export default React.ComponentType<{
-    parentCount: number;
-    nav: (url: string) => void;
-  }>;
+  export default React.ComponentType<import("./types").RemoteProps>;
 }

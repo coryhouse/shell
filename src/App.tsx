1146d946571454fc7ae2bfc6d8c9335103cf4ff7
Cory House <housecor@gmail.com>
2023-06-25 10:49:50 -0500

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense, lazy, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { ShellLayout } from "./components/ShellLayout";
+import { Routes, Route, Link } from "react-router-dom";
 import { User } from "./types/types";
 
 // Lazy load remotes so the initial page load only loads immediately necessary remotes.
@@ -12,43 +11,40 @@ const RemoteTwo = lazy(() => import("remote2/RemoteTwo"));
 const urls: Record<"about" | "home", string> = { about: "/about", home: "/" };
 const user: User = { id: 1, name: "John Doe" };
 
+var buildDate = process.env.BUILD_DATE;
+
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ShellLayout
-              count={count}
-              setCount={setCount}
-              RemoteOne={
-                <Suspense fallback="Loading...">
-                  <RemoteOne
-                    parentCount={count}
-                    urls={urls}
-                    user={user}
-                    baseUrl="/remote1"
-                  />
-                </Suspense>
-              }
-              RemoteTwo={
-                <Suspense fallback="Loading...">
-                  <RemoteTwo
-                    parentCount={count}
-                    urls={urls}
-                    user={user}
-                    baseUrl="/remote2"
-                  />
-                </Suspense>
-              }
-            />
-          }
-        />
+      <header style={{ backgroundColor: "lightblue" }}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/remote1">Remote 1</Link>
+            </li>
+            <li>
+              <Link to="/remote2">Remote 2</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-        {/* These routes support running each remote directly, without the shell visible, but with the shell's props still provided. */}
+        <p>Shell Build Date: {buildDate}</p>
+        <p>Shell count: {count}</p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increment shell counter
+        </button>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
         <Route
           path="remote1/*"
           element={

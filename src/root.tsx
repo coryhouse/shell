e3a5229b21b161539271136dbb6c0c3c3c3aa7e2
Cory House <housecor@gmail.com>
2023-07-01 12:18:55 -0500

    add error boundary

--- src/root.tsx
+++ src/root.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ErrorBoundary fallbackRender={(props) => <h1>Sorry, an error occurred.</h1>}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );

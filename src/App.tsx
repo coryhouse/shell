7644ec96694ff0354d711d8efc0353ac2efa9889
Cory House <housecor@gmail.com>
2023-07-02 16:13:42 -0500

    Extract routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { User } from "./types/types";
-import { remotes } from "./services/remote.service";
 import { getUser } from "./services/user.service";
+import { ShellLayout } from "./ShellLayout";
+import { ShellRoutes } from "./Routes";
 
 // TODO: Fetch these in real app.
 const urls: Record<"about" | "home", string> = { about: "/about", home: "/" };
-
-var buildDate = process.env.BUILD_DATE;
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -22,60 +20,14 @@ export default function App() {
 
   return (
     <>
-      <header style={{ backgroundColor: "lightblue" }}>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            {remotes.map(({ navLink }) => (
-              <li key={navLink.text}>
-                <Link to={navLink.path}>{navLink.text}</Link>
-              </li>
-            ))}
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <select value={account} onChange={(e) => setAccount(e.target.value)}>
-          <option value="account-one">Account One</option>
-          <option value="account-two">Account Two</option>
-        </select>
-
-        <p>Shell Build Date: {buildDate}</p>
-        <p>Shell count: {count}</p>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment shell count
-        </button>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<h2>Home Page</h2>} />
-        <Route path="about" element={<h2>About</h2>} />
-        {remotes.map(({ navLink, lazy }) => {
-          const LazyRemote = lazy;
-          return (
-            <Route
-              key={navLink.path}
-              path={navLink.path + "/*"}
-              element={
-                <Suspense fallback="Loading...">
-                  <LazyRemote
-                    parentCount={count}
-                    account={account}
-                    urls={urls}
-                    user={user}
-                    baseUrl={navLink.path}
-                  />
-                </Suspense>
-              }
-            />
-          );
-        })}
-        <Route path="*" element={<h2>404 - Not Found</h2>} />
-      </Routes>
+      <ShellLayout
+        account={account}
+        setAccount={setAccount}
+        count={count}
+        setCount={setCount}
+      >
+        <ShellRoutes account={account} count={count} urls={urls} user={user} />
+      </ShellLayout>
     </>
   );
 }

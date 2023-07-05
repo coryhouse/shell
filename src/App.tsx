658dd4f08c777bb9a5e027b92a3f334358db9647
Cory House <housecor@gmail.com>
2023-07-04 19:19:23 -0500

    render shell alone

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { User } from "./types/types";
 import { getUser } from "./services/user.service";
 import { ShellLayout } from "./ShellLayout";
 import { ShellRoutes } from "./Routes";
+import { useSearchParams } from "react-router-dom";
 
 // TODO: Fetch these in real app.
 const urls: Record<"about" | "home", string> = { about: "/about", home: "/" };
@@ -11,6 +12,7 @@ export default function App() {
   const [count, setCount] = useState(0);
   const [user, setUser] = useState<User | null>(null);
   const [account, setAccount] = useState("account-one");
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     getUser().then((user) => setUser(user));
@@ -18,14 +20,27 @@ export default function App() {
 
   if (!user) return <h1>Loading...</h1>;
 
+  const showShell = searchParams.get("shell") !== "n";
+
   return (
-    <ShellLayout
-      account={account}
-      setAccount={setAccount}
-      count={count}
-      setCount={setCount}
-    >
-      <ShellRoutes account={account} count={count} urls={urls} user={user} />
-    </ShellLayout>
+    <>
+      {showShell ? (
+        <ShellLayout
+          account={account}
+          setAccount={setAccount}
+          count={count}
+          setCount={setCount}
+        >
+          <ShellRoutes
+            account={account}
+            count={count}
+            urls={urls}
+            user={user}
+          />
+        </ShellLayout>
+      ) : (
+        <ShellRoutes account={account} count={count} urls={urls} user={user} />
+      )}
+    </>
   );
 }

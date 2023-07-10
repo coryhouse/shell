4ea9febcbc203af97b8d6c8f2d05b245d4ec53f1
Cory House <housecor@gmail.com>
2023-07-10 07:27:53 -0500

    add regenerator runtime

--- src/root.tsx
+++ src/root.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "./ErrorFallback";
+import "regenerator-runtime";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>

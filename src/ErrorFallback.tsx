216b7a3544870d92332613a87abe4b9937bc62a4
Cory House <housecor@gmail.com>
2023-07-13 11:16:03 -0500

    enhance fallback

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -1,11 +1,32 @@
 import { FallbackProps } from "react-error-boundary";
 
-export function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+interface ErrorFallbackProps extends FallbackProps {
+  /** Message to display above the error details. */
+  message?: string;
+
+  /** When true, call location.reload to completely reload everything. This is necessary when a remote fails to load. */
+  hardReloadWhenTryAgainIsClicked?: boolean;
+}
+
+export function ErrorFallback({
+  error,
+  resetErrorBoundary,
+  message = "Oops! Something went wrong.",
+  hardReloadWhenTryAgainIsClicked = false,
+}: ErrorFallbackProps) {
   return (
     <div role="alert">
-      <p>Oops! Something went wrong.</p>
+      <p>{message}</p>
       <pre style={{ color: "red" }}>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+      <button
+        onClick={
+          hardReloadWhenTryAgainIsClicked
+            ? () => location.reload()
+            : resetErrorBoundary
+        }
+      >
+        Try again
+      </button>
     </div>
   );
 }
